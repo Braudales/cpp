5f583f129ef408c2d4bfd832d9e85e1ef8d2b52d
Brayan Raudales <Brayanmejia28@gmail.com>
2021-02-16 20:58:07 -0600

    proyecto-1

--- Ejercicios/Proyecto1/main.cpp
+++ Ejercicios/Proyecto1/main.cpp
@@ -8,8 +8,42 @@ int main(int argc, char const *argv[])
 {
     system("cls");
 
-    // starShip();
-    // snake();
+ int opcion = 0;
+
+    while(true) {
+        system("cls");
+
+        cout << "Menu de videos Juegos"; 
+        cout << endl;
+        cout << "*********************";
+        cout << endl;
+        cout << endl;
+        cout<<"ingrese un numero para jugar"<<endl;
+        cout << "1 - Starship" << endl;
+        cout << "2 - Snake" << endl;
+        cout << "0 - Salir" << endl;
+        cout << "Ingrese una opcion del menu: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1: 
+           starShip();            
+            break;
+        case 2:
+             snake();
+            break;
+        default:
+            break;
+        }
+
+        system("pause");
+        cout << endl;
+
+        if (opcion == 0) {
+            break;
+        }
+    }
 
     return 0;
 }
